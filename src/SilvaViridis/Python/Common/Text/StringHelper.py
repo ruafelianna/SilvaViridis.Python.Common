b558c568acd3c2656b12d6779a3b959f5fec0057
Maria Sorokina <ruafelianna@ya.ru>
2025-03-18 13:24:49 +0300

    Add negation for string helper functions for better static analyzer experience

--- src/SilvaViridis/Python/Common/Text/StringHelper.py
+++ src/SilvaViridis/Python/Common/Text/StringHelper.py
@@ -1,12 +1,26 @@
+from typing import TypeGuard
+
 class StringHelper:
+    @staticmethod
+    def is_not_none_and_not_empty(
+            value : str | None,
+        ) -> TypeGuard[str]:
+            return value is not None and len(value) > 0
+
+    @staticmethod
+    def is_not_none_and_not_whitespace(
+            value : str | None,
+        ) -> TypeGuard[str]:
+            return StringHelper.is_not_none_and_not_empty(value) and not value.isspace()
+
     @staticmethod
     def is_none_or_empty(
             value : str | None,
         ) -> bool:
-            return value is None or len(value) == 0
+            return not StringHelper.is_not_none_and_not_empty(value)
 
     @staticmethod
     def is_none_or_whitespace(
             value : str | None,
         ) -> bool:
-            return StringHelper.is_none_or_empty(value) or value.isspace() # type: ignore[reportOptionalMemberAccess] - always not None
+            return not StringHelper.is_not_none_and_not_whitespace(value)
