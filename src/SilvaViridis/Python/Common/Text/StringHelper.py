a6bcaf296e1188cf82fb95ed29512f5c8d00e73d
Maria Sorokina <ruafelianna@ya.ru>
2025-03-24 09:29:49 +0300

    Add a method that returns empty string instead of 'None' when the value is None

--- src/SilvaViridis/Python/Common/Text/StringHelper.py
+++ src/SilvaViridis/Python/Common/Text/StringHelper.py
@@ -1,26 +1,39 @@
+from collections.abc import Callable
 from typing import TypeGuard
 
 class StringHelper:
     @staticmethod
     def is_not_none_and_not_empty(
-            value : str | None,
-        ) -> TypeGuard[str]:
-            return value is not None and len(value) > 0
+        value : str | None,
+    ) -> TypeGuard[str]:
+        return value is not None and len(value) > 0
 
     @staticmethod
     def is_not_none_and_not_whitespace(
-            value : str | None,
-        ) -> TypeGuard[str]:
-            return StringHelper.is_not_none_and_not_empty(value) and not value.isspace()
+        value : str | None,
+    ) -> TypeGuard[str]:
+        return StringHelper.is_not_none_and_not_empty(value) and not value.isspace()
 
     @staticmethod
     def is_none_or_empty(
-            value : str | None,
-        ) -> bool:
-            return not StringHelper.is_not_none_and_not_empty(value)
+        value : str | None,
+    ) -> bool:
+        return not StringHelper.is_not_none_and_not_empty(value)
 
     @staticmethod
     def is_none_or_whitespace(
-            value : str | None,
-        ) -> bool:
-            return not StringHelper.is_not_none_and_not_whitespace(value)
+        value : str | None,
+    ) -> bool:
+        return not StringHelper.is_not_none_and_not_whitespace(value)
+
+    @staticmethod
+    def to_str_empty_if_none[T](
+        value : T | None,
+        to_str : Callable[[T], str] | None = None,
+    ) -> str:
+        if value is None:
+            return ""
+        elif to_str is None:
+            return str(value)
+        else:
+            return to_str(value)
--- tests/Text/test_string_helper.py
+++ tests/Text/test_string_helper.py
@@ -1,5 +1,7 @@
 import pytest
 
+from typing import Any, Callable
+
 from SilvaViridis.Python.Common.Text import StringHelper as SH
 
 @pytest.mark.parametrize("value,expected", [
@@ -23,3 +25,20 @@ def test_none_or_empty(value : str | None, expected : bool):
 ])
 def test_none_or_whitespace(value : str | None, expected : bool):
     assert SH.is_none_or_whitespace(value) == expected
+
+def to_str_1(value : int) -> str:
+    return str(value + 1)
+
+@pytest.mark.parametrize("value,to_str,expected", [
+    (None, None, ""),
+    ("", None, ""),
+    ("    ", None, "    "),
+    ("\t\t\t", None, "\t\t\t"),
+    ("\n\n\n", None, "\n\n\n"),
+    ("aaa", None, "aaa"),
+    (12345, None, "12345"),
+    (None, str, ""),
+    (12345, to_str_1, "12346"),
+])
+def test_to_str_empty_if_none(value : Any, to_str : Callable[[Any], str], expected : str):
+    assert SH.to_str_empty_if_none(value, to_str) == expected
