cf15449466496b30f6f7be72bb9133b5eb100004
Maria Sorokina <ruafelianna@ya.ru>
2025-04-01 14:22:48 +0300

    Deprecate create_validator function; add new validator creation functions instead

--- src/SilvaViridis/Python/Common/Validation/_Utils.py
+++ src/SilvaViridis/Python/Common/Validation/_Utils.py
@@ -1,6 +1,13 @@
+from collections.abc import  Callable
+from enum import Enum
 from pydantic import AfterValidator, validate_call
 from typing import Any
+from warnings import deprecated
 
+from SilvaViridis.Python.Common.Collections import NonEmptySequence
+from SilvaViridis.Python.Common.Text import NonEmptyString
+
+@deprecated("Use the new functions of this module instead")
 @validate_call
 def create_validator(*types : type, strict : bool = False):
     def validate(value : Any) -> Any:
@@ -14,4 +21,101 @@ def create_validator(*types : type, strict : bool = False):
                     continue
         raise ValueError("No matching type found")
 
+    return AfterValidator(validate)
+
+class CheckMode(Enum):
+    all = 0
+    any = 1
+
+@validate_call
+def validate_is_instance[T](
+    value : T,
+    types : NonEmptySequence[type],
+    strict : bool,
+    msg : str,
+) -> T:
+    for t in types:
+        if isinstance(value, t):
+            return value
+        if not strict:
+            try:
+                return t(value)
+            except (TypeError, ValueError):
+                continue
+    raise ValueError(msg)
+
+@validate_call
+def validate_all_or_any[TObj, TElem](
+    obj : TObj,
+    collection : NonEmptySequence[TElem],
+    condition : Callable[[TObj, TElem], bool],
+    mode : CheckMode,
+    msg : str,
+) -> TObj:
+    ex = ValueError(msg)
+    for elem in collection:
+        result = condition(obj, elem)
+        if mode == CheckMode.all and not result:
+            raise ex
+        elif mode == CheckMode.any and result:
+            return obj
+    if mode == CheckMode.all:
+        return obj
+    else:
+        raise ex
+
+@validate_call
+def create_validator__is_instance(
+    types : NonEmptySequence[type],
+    strict : bool = False,
+):
+    @validate_call
+    def validate[T](value : T) -> T:
+        return validate_is_instance(
+            value,
+            types,
+            strict = strict,
+            msg = "No matching type found",
+        )
+    return AfterValidator(validate)
+
+@validate_call
+def create_validator__has_attributes(
+    attrs : NonEmptySequence[NonEmptyString],
+    mode : CheckMode = CheckMode.all,
+):
+    @validate_call
+    def validate[T](value : T) -> T:
+        @validate_call
+        def check_has_attr(obj : T, elem : str):
+            return hasattr(obj, elem)
+
+        return validate_all_or_any(
+            value,
+            attrs,
+            check_has_attr,
+            mode,
+            "The object doesn't contain all required attributes",
+        )
+    return AfterValidator(validate)
+
+@validate_call
+def create_validator__implements_magic_methods(
+    methods : NonEmptySequence[NonEmptyString],
+    mode : CheckMode = CheckMode.all,
+):
+    @validate_call
+    def validate[T](value : T) -> T:
+        @validate_call
+        def check_magic_methods(obj : T, elem : str):
+            method = getattr(obj, elem, None)
+            return method is not None and method != getattr(object, elem, None)
+
+        return validate_all_or_any(
+            value,
+            methods,
+            check_magic_methods,
+            mode,
+            "The object doesn't implement all required magic methods",
+        )
     return AfterValidator(validate)
--- src/SilvaViridis/Python/Common/Validation/__init__.py
+++ src/SilvaViridis/Python/Common/Validation/__init__.py
@@ -1,5 +1,19 @@
-from ._Utils import create_validator
+from ._Utils import (
+    CheckMode,
+    create_validator,
+    create_validator__is_instance,
+    create_validator__has_attributes,
+    create_validator__implements_magic_methods,
+    validate_all_or_any,
+    validate_is_instance,
+)
 
 __all__ = [
+    "CheckMode",
     "create_validator",
+    "create_validator__is_instance",
+    "create_validator__has_attributes",
+    "create_validator__implements_magic_methods",
+    "validate_all_or_any",
+    "validate_is_instance",
 ]
